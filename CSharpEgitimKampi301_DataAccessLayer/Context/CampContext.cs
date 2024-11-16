42654c6f8ba78b20407a5eab987b29b53d2ca4cc
fatihademtas0 <fatihademtas@outlook.com>
2024-11-16 22:37:36 +0300

    first commit

---
+++ CSharpEgitimKampi301_DataAccessLayer/Context/CampContext.cs
@@ -0,0 +1,19 @@
+﻿using CSharpEgitimKampi301_EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301_DataAccessLayer.Context
+{
+	public class CampContext: DbContext
+	{
+        public DbSet<Category> Categories { get; set; } // dbsetin içindeki c#da kullanılacak sınıf , categories ise sqldeki tablo ismi
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
---
+++ CSharpEgitimKampi301_EntityLayer/Concrete/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301_EntityLayer.Concrete
+{
+	public class Product
+	{
+		public int ProductId { get; set; }
+		public string ProductName { get; set; }
+		public int ProductStock {  get; set; }
+		public decimal ProductPrice { get; set; }
+		public string ProdcutDescription { get; set; }
+		public int CategoryId { get; set; } // buraya tekil ekleedik çünkü her ürünün sadece 1 ıd si olabilir
+		public virtual Category Category { get; set; }
+		public List<Order> Orders { get; set; }
+	}
+}
